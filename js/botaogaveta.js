252155f738bdf1e7057a76c714c59758bb793784
Maria Giulia <msgiuliamartins@gmail.com>
2025-02-25 00:13:00 -0300

    pag giulia pronta

--- js/botaogaveta.js
+++ js/botaogaveta.js
@@ -3,10 +3,15 @@ const accordions = document.querySelectorAll('.accordion');
 accordions.forEach(acord => {
     acord.addEventListener('click', () => {
         const accordionBody = acord.querySelector('.accordion-body');
+        const header = acord.querySelector('.accordion-header');
+        const arrow = acord.querySelector('.arrow');
+
+        // Alternar a classe active no corpo do accordion
         accordionBody.classList.toggle('active');
+        header.classList.toggle('active');
+        acord.classList.toggle('active');
 
         // Alternar a rotação da seta
-        const arrow = acord.querySelector('.arrow');
         arrow.style.transform = accordionBody.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0deg)';
     });
 });
